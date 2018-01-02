0c79d208d861ec519929e0f0cc874ac6d70878b3
Sean Oughton <seanoughton@gmail.com>
2018-01-02 18:32:02 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('#pic-list li:last img')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.')
+}
